d98039f3d63f1c731c8641be1135ecc24cabc4ba
JakeEllis <jmelli12@gmail.com>
2013-06-22 22:24:30 -0400

    Added EDMX and made some repo changes

---
+++ KYSI_Pledge_Points_System/SAE_DAL/SAEDB.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Jake Ellis\Desktop\KYSI_SAE\KYSI_Pledge_Points_System\SAE_DAL\SAEDB.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
