3434a77da12be0ac369ba58f633308d1daa45e9b
JakeEllis <jmelli12@gmail.com>
2013-06-22 21:58:47 -0400

    Added repo and SQL DB

---
+++ KYSI_Pledge_Points_System/SAE_DAL/Repositories/PledgeClassRepo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SAE_DAL.Repositories
+{
+    public class PledgeClassRepo
+
+    {
+        public void Get_Class_Name()
+        {
+
+        }
+
+
+        public void Get_Class_Semester()
+        {
+
+        }
+
+        public void Get_Class_Year()
+        {
+
+        }
+
+        public void Get_Pledge_ED()
+        {
+
+        }
+        public void Get_Co_Pledge()
+        {
+
+        }
+
+        public void Get_Pledge_Count()
+        {
+
+        }
+
+        public void Get_Initiated_Count()
+        {
+
+        }
+
+        public void Get_Pledge_Drop_Count()
+        {
+
+        }
+
+    }
+}
