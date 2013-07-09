88d7328f0b11146bab8ba419e115fd09cec81d57
JakeEllis <jmelli12@gmail.com>
2013-07-08 22:50:35 -0400

    Change the layout and index file. added banner photo

--- SAE_KYSI_PLEDGE/SAE_KYSI_PLEDGE/Controllers/HomeController.cs
+++ SAE_KYSI_PLEDGE/SAE_KYSI_PLEDGE/Controllers/HomeController.cs
@@ -10,21 +10,21 @@ namespace SAE_KYSI_PLEDGE.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Home Controller file";
 
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "Home Controller file";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Home Controller file";
 
             return View();
         }
