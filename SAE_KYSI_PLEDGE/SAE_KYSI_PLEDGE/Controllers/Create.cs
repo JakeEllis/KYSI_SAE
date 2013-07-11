419f0a4635d85893878822bcc9bf11da37e0d53c
JakeEllis <jmelli12@gmail.com>
2013-07-10 20:22:35 -0400

    Trying to add links. Evan look at this

---
+++ SAE_KYSI_PLEDGE/SAE_KYSI_PLEDGE/Controllers/Create.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAE_KYSI_PLEDGE.Controllers
+{
+    public class Create : Controller
+    {
+        public ActionResult CreateClass()
+        {
+            ViewBag.Message = "Create Pledge Class";
+
+            return View();
+        }
+
+        public ActionResult CreatePledge()
+        {
+            ViewBag.Message = "Create Pledge";
+
+            return View();
+        }
+
+    }
+}
