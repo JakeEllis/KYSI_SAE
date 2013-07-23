7d84035e9ef85d5224080b422430fd399e7c880b
JakeEllis <jmelli12@gmail.com>
2013-07-22 22:40:38 -0400

    Added View model files

---
+++ SAE_KYSI_PLEDGE/SAE_KYSI_PLEDGE/ViewModels/EditClassViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SAE_CORE.Models;
+using SAE_DAL.Repositories;
+
+namespace SAE_KYSI_PLEDGE.ViewModels
+{
+    public class EditClassViewModel
+    {
+        //public SelectList Classes { get; private set; }
+        //PledgeClassRepo Repo = new PledgeClassRepo();
+        //PledgeClassModel Pledge = new PledgeClassModel();
+
+       //public EditClassViewModel(PledgeClassModel pledge)
+       //{
+       //    Pledge = pledge;
+       //    Classes = new SelectList(pledge.Pledge_Class_Name);
+       //}
+
+       // [Authorize]
+       // public ActionResult EditClass(string className)
+       // {
+       //     SAE_KYSI_PLEDGE Pledge = 
+       //     PledgeClassRepo Repo = Repo.getById(Pledge_Class_Name);
+       // }
+
+    }
+}
