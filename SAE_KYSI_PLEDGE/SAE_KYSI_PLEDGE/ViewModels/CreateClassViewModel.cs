e7d025bfb638585ef9d04dcebc2a269ccfa7b48c
JakeEllis <jmelli12@gmail.com>
2013-11-13 22:06:53 -0500

    Changes

--- SAE_KYSI_PLEDGE/SAE_KYSI_PLEDGE/ViewModels/CreateClassViewModel.cs
+++ SAE_KYSI_PLEDGE/SAE_KYSI_PLEDGE/ViewModels/CreateClassViewModel.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SAE_CORE.Models;
+
 
 namespace SAE_KYSI_PLEDGE.ViewModels
 {
     public class CreateClassViewModel : Controller
     {
-    //    //
-    //    // GET: /CreateClassViewModel/
+        //
+        // GET: /CreateClassViewModel/
 
-    //    public ActionResult Index()
-    //    {
-    //        return View();
-    //    }
+        List<PledgeClassModel> PledgeClassModel { get; set; }
+        List<> PledgeModel { get; set; }
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
 
     }
 }
